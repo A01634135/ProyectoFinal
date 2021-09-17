e3dc4ac32afa82f0c4e2f0b6c375931294ea7d38
A01634135 <A01634135@itesm.mx>
2021-09-17 06:27:34 -0500

    Agregar documentacion y comentarios

--- Programa/color.py
+++ Programa/color.py
@@ -1,16 +1,25 @@
+"""
+Programa que despliega un menu para encontrar los valores minimos y maximos de HSV de un color.
+Recibe un video en vivo y despliega un menu con trackbars, y 3 ventanas con el video original, una mascara y su resultado bitwise
+"""
+
+
 import cv2
 import numpy as np
  
 frame_largo = 200
 frame_alto = 200
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #recibe video de la camara default
 cap.set(3, frame_largo)
 cap.set(4, frame_alto)
  
- 
+
+""" Funcion que no regresa nada """
 def empty(a):
     pass
- 
+
+
+""" Crea una ventana con menu en el que hay 6 trackbars  """
 cv2.namedWindow("HSV")
 cv2.resizeWindow("HSV", 640, 240)
 cv2.createTrackbar("HUE Min", "HSV", 0, 179, empty)
@@ -24,7 +33,7 @@ cv2.createTrackbar("VAL Max", "HSV", 255, 255, empty)
 while True:
  
     exito, imag = cap.read()
-    imgHsv = cv2.cvtColor(imag, cv2.COLOR_BGR2HSV)
+    imgHsv = cv2.cvtColor(imag, cv2.COLOR_BGR2HSV) #convierte la imagen de color a HSV
  
     h_min = cv2.getTrackbarPos("HUE Min", "HSV")
     h_max = cv2.getTrackbarPos("HUE Max", "HSV")
@@ -35,15 +44,16 @@ while True:
  
     ini = np.array([h_min, s_min, v_min])
     fin = np.array([h_max, s_max, v_max])
-    mask = cv2.inRange(imgHsv, ini, fin)
-    res = cv2.bitwise_and(imag, imag, mask = mask)
+    mask = cv2.inRange(imgHsv, ini, fin) #crea una mascara con los valores HSV
+    res = cv2.bitwise_and(imag, imag, mask = mask) #conjuncion and de la mascara e imagen
  
     mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
     h_stack = np.hstack([imag, mask, res])
-    cv2.imshow('Horizontal Stacking', h_stack)
+    cv2.imshow('Horizontal Stacking', h_stack) #muestra ventana con las versiones de la imagen
     
-    if cv2.waitKey(1) and 0xFF == ord('p'):
+    if cv2.waitKey(1) and 0xFF == ord('p'): #termina el programa
         break
  
+
 cap.release()
 cv2.destroyAllWindows()
--- Programa/dibujo.py
+++ Programa/dibujo.py
@@ -1,31 +1,52 @@
+"""
+Programa que define un punto medio en el contorno de un color y dibuja con él en la pantalla.
+Recibe un video en vivo y muestra puntos continuos del color que perciba.
+"""
+
+
 import cv2
 import numpy as np
 
-
 frameWidth = 640
 frameHeight = 480
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #recibe video de la camara default
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
  
-color_hsv = [[0, 191, 0, 22, 255, 255],
-            [34, 91, 80, 179, 255, 255]]
-color_bgr = [[51, 153, 255], #naranja
-             [51, 255, 51]] #verde
+color_hsv = [[0, 191, 0, 22, 255, 255], #naranja en HSV
+            [34, 91, 80, 179, 255, 255]] #verde en HSV
+color_bgr = [[51, 153, 255], #naranja en BGR
+             [51, 255, 51]] #verde en BGR
 puntos =  []
  
 
+"""" Encuentra el contorno de un area en una imagen """
+def encontrar_contorno(imag):
+    contornos, hierarchy = cv2.findContours(imag, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    x, y, w, h = 0, 0, 0, 0
+
+    for contor in contornos: #aproxima el contorno de la figura
+        area = cv2.contourArea(contor)
+        if area > 500:
+            perim = cv2.arcLength(contor, True)
+            aprox = cv2.approxPolyDP(contor, 0.02 * perim, True)
+            x, y, w, h = cv2.boundingRect(aprox)
+
+    return x + w // 2, y #obtiene el punto medio en el punto alto del contorno
+
+
+""" Encuentra el color que se muestra en la pantalla """
 def encontrar_color(imag, color_hsv, color_bgr):
 
-    imag_hsv = cv2.cvtColor(imag, cv2.COLOR_BGR2HSV)
+    imag_hsv = cv2.cvtColor(imag, cv2.COLOR_BGR2HSV) #convierte la imagen de BGR a HSV
     cont = 0
     puntos2 = []
 
     for color in color_hsv:
         ini = np.array(color[0:3])
         fin = np.array(color[3:6])
-        mask = cv2.inRange(imag_hsv, ini, fin)
+        mask = cv2.inRange(imag_hsv, ini, fin) #crea una mascara con los valores HSV
         x, y = encontrar_contorno(mask)
         cv2.circle(imag_final, (x, y), 15, color_bgr[cont], cv2.FILLED)
         
@@ -36,20 +57,7 @@ def encontrar_color(imag, color_hsv, color_bgr):
     return puntos2
  
 
-def encontrar_contorno(imag):
-    contornos, hierarchy = cv2.findContours(imag, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    x, y, w, h = 0, 0, 0, 0
-
-    for contor in contornos:
-        area = cv2.contourArea(contor)
-        if area > 500:
-            perim = cv2.arcLength(contor, True)
-            aprox = cv2.approxPolyDP(contor, 0.02 * perim, True)
-            x, y, w, h = cv2.boundingRect(aprox)
-
-    return x + w // 2, y #obtiene el punto medio en el punto alto
- 
-
+""" Dibuja puntos continuos segun el color en la pantalla """
 def drawOnCanvas(puntos,color_bgr):
     for punt in puntos:
         cv2.circle(imag_final, (punt[0], punt[1]), 10, color_bgr[punt[2]], cv2.FILLED)
@@ -67,5 +75,10 @@ while True:
         drawOnCanvas(puntos, color_bgr)
  
     cv2.imshow("Result", imag_final)
-    if cv2.waitKey(1) and 0xFF == ord('p'):
+
+    if cv2.waitKey(1) and 0xFF == ord('p'): #termina el programa
         break
+
+
+cap.release()
+cv2.destroyAllWindows()
